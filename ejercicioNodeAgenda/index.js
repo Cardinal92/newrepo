02cbdc1b26d60f4945cf3148b30ac0bb86a4be82
Martin <rioshernanmartin@gmail.com>
2024-04-06 18:54:25 -0300

    ssssssss

--- ejercicioNodeAgenda/index.js
+++ ejercicioNodeAgenda/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
+const cors = require("cors")
 app.use(express.json())
+app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 morgan.token("body", (req) => JSON.stringify(req.body))
