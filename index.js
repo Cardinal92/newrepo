57e91dc2707c67225afde3a5748c435ae4e5720a
Martin <rioshernanmartin@gmail.com>
2024-04-11 18:48:21 -0300

    fullsex

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 app.use(express.json())
 app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(express.static("dist"))
 
 morgan.token("body", (req) => JSON.stringify(req.body))
 
